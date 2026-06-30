6822757beedc0e44a37bdb45683e88994bc74e04
AlexL <Alex.carlon26@gmail.com>
2026-06-30 16:27:55 -0700

    Initial scaffold with DI wiring for Graph and HttpClient. Needs much more work but a good stopping point for initial commit.

---
+++ SnipeITSyncFormerEmployees/FormerEmployeeSync.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Graph;
+
+namespace SnipeITSyncFormerEmployees;
+
+public class FormerEmployeeSync
+{
+    private readonly ILogger<FormerEmployeeSync> _logger;
+    private readonly HttpClient _httpClient;
+    private readonly GraphServiceClient _graphServiceClient;
+
+    public FormerEmployeeSync(ILogger<FormerEmployeeSync> logger, HttpClient httpClient,
+        GraphServiceClient graphServiceClient)
+    {
+        _logger = logger;
+        _httpClient = httpClient;
+        _graphServiceClient = graphServiceClient;
+    }
+    
+    [Function("FormerEmployeeSync")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to the new Azure Functions!");
+    }
+    
+
+}
--- SnipeITSyncFormerEmployees/Program.cs
+++ SnipeITSyncFormerEmployees/Program.cs
@@ -1,17 +1,28 @@
+using System;
+using Azure.Identity;
 using Azure.Monitor.OpenTelemetry.Exporter;
-using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Azure.Functions.Worker.OpenTelemetry;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using OpenTelemetry;
+using Microsoft.Graph;
 
 var builder = FunctionsApplication.CreateBuilder(args);
+builder.Services.AddHttpClient();
+
+builder.Services.AddSingleton(sp =>
+{
+    var tenantId = Environment.GetEnvironmentVariable("AZURE_TENANT_ID");
+    var clientId = Environment.GetEnvironmentVariable("AZURE_CLIENT_ID");
+    var clientSecret = Environment.GetEnvironmentVariable("AZURE_CLIENT_SECRET");
+
+    if (tenantId is null || clientId is null || clientSecret is null)
+        throw new InvalidOperationException("Missing one or more Azure AD environment variables.");
+
+    var credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+    return new GraphServiceClient(credential, new[] { "https://graph.microsoft.com/.default" });
+});
 
 builder.ConfigureFunctionsWebApplication();
-
-builder.Services.AddOpenTelemetry()
-    .UseFunctionsWorkerDefaults()
-    .UseAzureMonitorExporter();
-
 builder.Build().Run();
+
